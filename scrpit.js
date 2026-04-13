916c92ada605ee35808aafa70e744f7e0433fe60
Ágatha <agatha.rigotti@escola.pr.gov.br>
2026-04-13 09:08:08 -0300

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,12 @@
+var aluno1 = "mayk";
+var notaAluno1 = 9.8;
+
+var aluno2 = "diego";
+var notaAluno2 = 10;
+
+var aluno3 = "Ana";
+var notaAluno3 = 0.0;
+
+const media = (notaAluno1 + notaAluno2 + notaAluno3 ) /3;
+
+alert(media);
