057e9f3af7a854b3733e1978fd538553f201b0bc
Ágatha <agatha.rigotti@escola.pr.gov.br>
2026-09-17 12:34:14 -0300

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -1,2 +1,8 @@
-let nome = "Ágatha";
-alert("Meu nome é " + nome);
+const SENHA_CORRETA = "alopiasvulpinus";
+let tentativa= prompt("Digite a senha para acessar a página:");
+if(tentativa !== SENHA_CORRETA) {
+    alert("Senha incorreta! acesso negado.");
+    window.location.href = "about:blank";
+} else {
+    alert("Olá, Bem vindo ao seu catálogo!");
+}
